ad81f1ef630c5d02d8477b5a2916489f9debc82f
ChiefMoneyBags <minemandev@gmail.com>
2020-03-21 17:47:17 -0400

    now implements Cancellable

--- TempFly/src/com/moneybags/tempfly/event/ActionBarSendEvent.java
+++ TempFly/src/com/moneybags/tempfly/event/ActionBarSendEvent.java
@@ -1,10 +1,11 @@
 package com.moneybags.tempfly.event;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class ActionBarSendEvent extends Event {
+public class ActionBarSendEvent extends Event implements Cancellable {
 	
 	private final HandlerList handlers = new HandlerList();
 	private final Player player;
@@ -37,10 +38,12 @@ public class ActionBarSendEvent extends Event {
 		this.message = message;
 	}
 
+	@Override
 	public boolean isCancelled() {
 		return cancelled;
 	}
 
+	@Override
 	public void setCancelled(boolean cancelled) {
 		this.cancelled = cancelled;
 	}
--- TempFly/src/com/moneybags/tempfly/event/TitleSendEvent.java
+++ TempFly/src/com/moneybags/tempfly/event/TitleSendEvent.java
@@ -1,14 +1,15 @@
 package com.moneybags.tempfly.event;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class TitleSendEvent extends Event{
+public class TitleSendEvent extends Event implements Cancellable {
 	private static final HandlerList handlers = new HandlerList();
 	private final Player player;
-	private String title;
-	private String subtitle;
+	private String
+		title, subtitle;
 	private boolean cancelled = false;
 
 	public TitleSendEvent(Player player, String title, String subtitle) {
@@ -46,10 +47,12 @@ public class TitleSendEvent extends Event{
 		this.subtitle = subtitle;
 	}
 
+	@Override
 	public boolean isCancelled() {
 		return cancelled;
 	}
 
+	@Override
 	public void setCancelled(boolean cancelled) {
 		this.cancelled = cancelled;
 	}
