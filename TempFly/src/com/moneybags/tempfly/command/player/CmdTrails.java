ff9592d4ce2a83732e3c49cb93e4ef07d83608ec
ChiefMoneyBags <minemandev@gmail.com>
2020-03-23 17:25:20 -0400

    open trails gui

---
+++ TempFly/src/com/moneybags/tempfly/command/player/CmdTrails.java
@@ -0,0 +1,24 @@
+package com.moneybags.tempfly.command.player;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.moneybags.tempfly.gui.GuiSession;
+import com.moneybags.tempfly.gui.pages.PageTrails;
+import com.moneybags.tempfly.util.U;
+import com.moneybags.tempfly.util.V;
+
+public class CmdTrails {
+
+	public CmdTrails(CommandSender s) {
+		if (!U.isPlayer(s)) {
+			U.m(s, V.invalidSender);
+			return;
+		}
+		if (!U.hasPermission(s, "tempfly.trails")) {
+			U.m(s, V.invalidPermission);
+			return;
+		}
+		new PageTrails(GuiSession.newGuiSession((Player)s), 0);
+	}
+}
