7d34e6bcc106a5a85584acbf38dbad9d5ad0d689
ChiefMoneyBags <minemandev@gmail.com>
2020-03-28 10:40:33 -0400

    im working on mysql.

---
+++ TempFly/src/com/moneybags/tempfly/util/Data.java
@@ -0,0 +1,26 @@
+package com.moneybags.tempfly.util;
+
+public class Data {
+
+	/**
+	private static String
+		ip,
+		port,
+		name,
+		user,
+		pass;
+
+	
+	public static Connection getConnection() {
+		try {
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://" + ip + ":" + port + "/" + name;
+			
+			Connection con = DriverManager.getConnection(url, user, pass);
+		} catch(Exception e) {
+			
+		}
+		return null;
+	}
+	*/
+}
