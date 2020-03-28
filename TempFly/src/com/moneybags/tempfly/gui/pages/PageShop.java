ed51edc00ddfb3c102022fadc5b4165cf9914995
ChiefMoneyBags <minemandev@gmail.com>
2020-03-28 10:40:42 -0400

    shop gui

---
+++ TempFly/src/com/moneybags/tempfly/gui/pages/PageShop.java
@@ -0,0 +1,167 @@
+package com.moneybags.tempfly.gui.pages;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import com.moneybags.tempfly.TempFly;
+import com.moneybags.tempfly.gui.DynamicPage;
+import com.moneybags.tempfly.gui.GuiSession;
+import com.moneybags.tempfly.time.TimeHandle;
+import com.moneybags.tempfly.util.CompatMaterial;
+import com.moneybags.tempfly.util.F;
+import com.moneybags.tempfly.util.U;
+import com.moneybags.tempfly.util.V;
+
+import net.milkbowl.vault.economy.Economy;
+
+public class PageShop extends DynamicPage {
+	
+	private static List<ShopOption> allOptions = new ArrayList<>();
+	
+	private static String title;
+	private static ItemStack background, toolbar, next, prev;
+	
+	public static void initialize() {
+		FileConfiguration config = F.page;
+		String path = "page.shop";
+		title = U.cc(config.getString(path + ".title", "&dParticle Trails"));
+		background = U.getConfigItem(config, path + ".background");
+		toolbar = U.getConfigItem(config, path + ".toolbar");
+		next = U.getConfigItem(config, path + ".next");
+		prev = U.getConfigItem(config, path + ".prev");
+		
+		CompatMaterial.setType(background, CompatMaterial.GRAY_STAINED_GLASS_PANE);
+		CompatMaterial.setType(toolbar, CompatMaterial.BLACK_STAINED_GLASS_PANE);
+		CompatMaterial.setType(next, CompatMaterial.REDSTONE_TORCH);
+		CompatMaterial.setType(prev, CompatMaterial.REDSTONE_TORCH);
+		
+		ConfigurationSection csOptons = F.config.getConfigurationSection("shop.options");
+		if (csOptons != null) {
+			for (String s: csOptons.getKeys(false)) {
+				path = "shop.options." + s;
+				allOptions.add(new ShopOption(F.config.getInt(path + ".time", 0), F.config.getDouble(path + ".cost", 1000000)));
+			}
+		}
+	}
+	
+	private Inventory inv;
+	private Map<Integer, ShopOption> layout = new HashMap<>();
+	
+	public PageShop(GuiSession session, int num) {
+		super(session);
+		
+		this.inv = Bukkit.createInventory(null, 54, title);
+		
+		for (int i = 0; i < 45; i++) {
+			inv.setItem(i, background);
+		}
+		for (int i = 45; i < 54; i++) {
+			inv.setItem(i, toolbar);
+		}
+		
+		super.calculateSlots(num, allOptions.size());
+		if (allOptions.size() < 21*num) {
+			num = 0;
+		}
+		List<ShopOption> options = new ArrayList<>();
+		options.addAll(allOptions);
+		options = options.subList(21 * num, options.size());
+		Iterator<ShopOption> ito = options.iterator();
+		Iterator<Integer> its = super.getOpenSlots().iterator();
+		while (its.hasNext() && ito.hasNext()) {
+			int slot = its.next();
+			ShopOption option = ito.next();
+			layout.put(slot, option);
+			inv.setItem(slot, option.getDisplay());
+		}
+		if (allOptions.size() > (getPageNumber()+1)*21) {
+			inv.setItem(53, next);
+		} 
+		if (getPageNumber() > 0) {
+			inv.setItem(45, prev);
+		}
+		
+		session.newPage(this, inv);
+	}
+	
+	@Override
+	public void runPage(int slot, InventoryClickEvent e) {
+		if (layout.containsKey(slot)) {
+			Player p = session.getPlayer();
+			Economy eco = TempFly.eco;
+			double balance = eco.getBalance(p);
+			ShopOption option = layout.get(slot);
+			if (option.getCost() > balance) {
+				U.m(p, TimeHandle.regexString(V.invalidFunds, option.getTime())
+						.replaceAll("\\{COST}", String.valueOf(option.getCost())));
+			} else {
+				eco.withdrawPlayer(p, option.getCost());
+				TimeHandle.addTime(p.getUniqueId(), option.getTime());
+				U.m(p, TimeHandle.regexString(V.timePurchased, option.getTime())
+						.replaceAll("\\{COST}", String.valueOf(option.getCost())));	
+			}
+		} else if (slot == 53 && allOptions.size() > (getPageNumber()+1)*21) {
+			new PageTrails(session, getPageNumber()+1);
+		} else if (slot == 45 && getPageNumber() > 0) {
+			new PageTrails(session, getPageNumber()-1);
+		}
+	}
+
+	public static class ShopOption {
+		
+		private int time;
+		private double cost;
+		private ItemStack item;
+		
+		private ShopOption(int time, double cost) {
+			this.time = time;
+			this.cost = cost;
+			item = CompatMaterial.get(CompatMaterial.FEATHER);
+			ItemMeta meta = item.getItemMeta();
+			
+			String name = U.cc(
+					TimeHandle.regexString(
+							F.page.getString("page.shop.option.name", "{FORMATTED_TIME}"), time)
+							.replaceAll("\\{COST}", String.valueOf(cost))
+								);
+			meta.setDisplayName(name);
+			
+			List<String> l = F.page.getStringList("page.shop.option.lore");
+			List<String> lore = new ArrayList<>();
+			if (l != null) {
+				for (String s: l) {
+					lore.add(U.cc(
+							TimeHandle.regexString(s, time)
+							.replaceAll("\\{COST}", String.valueOf(cost))
+							));
+				}
+			}
+			meta.setLore(lore);
+			item.setItemMeta(meta);
+		}		
+		public int getTime() {
+			return time;
+		}
+		
+		public double getCost() {
+			return cost;
+		}
+		
+		public ItemStack getDisplay() {
+			return item;
+		}
+	}
+	
+}
