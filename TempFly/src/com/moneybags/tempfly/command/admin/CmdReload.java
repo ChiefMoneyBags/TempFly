21bb156fd6268c33a1790dbc1c42ef49324f18ef
ChiefMoneyBags <minemandev@gmail.com>
2020-06-19 19:53:39 -0400

    Added reload capability for the gui shop and gui trails selector

--- TempFly/src/com/moneybags/tempfly/command/admin/CmdReload.java
+++ TempFly/src/com/moneybags/tempfly/command/admin/CmdReload.java
@@ -3,6 +3,9 @@ package com.moneybags.tempfly.command.admin;
 import org.bukkit.command.CommandSender;
 
 import com.moneybags.tempfly.TempFly;
+import com.moneybags.tempfly.gui.GuiSession;
+import com.moneybags.tempfly.gui.pages.PageShop;
+import com.moneybags.tempfly.gui.pages.PageTrails;
 import com.moneybags.tempfly.util.F;
 import com.moneybags.tempfly.util.U;
 import com.moneybags.tempfly.util.V;
@@ -17,5 +20,8 @@ public class CmdReload {
 		U.m(s, V.reload);
 		F.createFiles(TempFly.plugin);
 		V.loadValues();
+		GuiSession.endAllSessions();
+		PageTrails.initialize();
+		PageShop.initialize();
 	}
 }
