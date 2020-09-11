da30f580e238492dbb7b48ae536a3d92415b8b58
ChiefMoneyBags <minemandev@gmail.com>
2020-09-10 20:43:26 -0400

    Hooked with bentoBox

---
+++ TempFly/src/com/moneybags/tempfly/hook/skyblock/plugins/BskyblockHook.java
@@ -0,0 +1,68 @@
+package com.moneybags.tempfly.hook.skyblock.plugins;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import com.moneybags.tempfly.TempFly;
+import com.moneybags.tempfly.util.Console;
+
+import world.bentobox.bentobox.BentoBox;
+import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bentobox.api.events.IslandBaseEvent;
+
+public class BskyblockHook extends BentoHook {
+
+	public static final String BSKYBLOCK = "BSkyBlock"; 
+	
+	
+	
+	public BskyblockHook(TempFly plugin) {
+		super(plugin);
+	}
+	
+	@Override
+	public String getHookName() {
+		return BSKYBLOCK;
+	}
+	
+	@Override
+	public String getConfigName() {
+		return BSKYBLOCK;
+	}
+	
+	@Override
+	public String getDataName() {
+		return BSKYBLOCK;
+	}
+	
+	@Override
+	public World getMainIslandWorld() {
+		Object bsky = getBSkyblockAddon();
+		if (bsky == null) {
+			Console.warn("The BskyBlock hook encountered a problem. It will now disable!");
+			setEnabled(false);
+			return null;
+		}
+		Class<?> clazz = bsky.getClass();
+		try {return (World) (clazz.getMethod("getOverWorld").invoke(bsky) != null ? clazz.getMethod("getOverWorld").invoke(bsky)
+					: clazz.getMethod("getNetherWorld").invoke(bsky) != null ? clazz.getMethod("getNetherWorld").invoke(bsky)
+							: clazz.getMethod("getEndWorld").invoke(bsky));} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public Object getBSkyblockAddon() {
+		Optional<Addon> addon = BentoBox.getInstance().getAddonsManager().getAddonByName("BSkyBlock");
+		return addon.isPresent() ? addon.get() : null;
+	}
+	
+
+
+}
